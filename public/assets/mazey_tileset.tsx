0b7e770761efedc8b3e0ea9ef58b0b9077ebba8c
Darion <noirad@gmail.com>
2024-08-02 18:13:43 -0400

    created tilemap assets

---
+++ public/assets/mazey_tileset.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="mazey_tileset" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <editorsettings>
+  <export target="mazey_tileset.json" format="json"/>
+ </editorsettings>
+ <image source="mazey_tileset.png" width="64" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
